7716536d3e940ba28f16b74dbfc95cce3ab91ae1
Jessica Gilleran <jess.gilleran@gmail.com>
2021-02-19 05:43:39 -0500

    Updated to provide UI

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const rockBtn = document.querySelector('.rock-button');
+rockBtn.addEventListener('click',function(){playRound('R')});
 
-//This function generates a random number between 1 and 3 inclusive
-//to make the computers choice
+const paperBtn = document.querySelector('.paper-button');
+paperBtn.addEventListener('click',function(){playRound('P')});
+
+const scissorsBtn = document.querySelector('.scissors-button');
+scissorsBtn.addEventListener('click',function(){playRound('S')});
+
+const roundOneResult = document.querySelector('.round-one-score');
+const roundTwoResult = document.querySelector('.round-two-score');
+const roundThreeResult = document.querySelector('.round-three-score');
+const roundFourResult = document.querySelector('.round-four-score');
+const roundFiveResult = document.querySelector('.round-five-score');
+
+const playerScore = document.querySelector('.player-score');
+const compScore = document.querySelector('.computer-score');
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -8,67 +23,106 @@ function getRandomInt(min, max) {
 }
 
 //This function randomly decides the computers choice
-function computerPlay() {
+function getComputerChoice() {
     //Determine the random number
     let randomNumber = getRandomInt(1,4);
     if (randomNumber === 1) {
-        return 'rock';
+        return 'R';
     } else if (randomNumber === 2) {
-        return 'paper';
+        return 'P';
     } else {
-        return 'scissors';
+        return 'S';
     }
 }
 
-//This function gets the users choice
-function userPlay() {
-    let validChoice = false;
-    let userChoice;
-
-    while (validChoice === false) {
-        userChoice = window.prompt("Choose rock, paper or scissors: ");
-        //Lower case user choice to account for all cases
-        userChoice = userChoice.toLowerCase();
-        if (userChoice === 'rock'|| userChoice === 'paper' || userChoice === 'scissors') {
-            validChoice = true;
-        } else {
-            alert("Incorrect input, enter rock, paper or scissors");
-        }
-    }
-    return userChoice;
-}
+let roundNum = 1;
+let playerTotal = 0;
+let compTotal = 0;
 
 //This function takes the computer and player choices and decides the winner
-function oneRound(compChoice,playerChoice) {
-    //Determine if a tie
-    if (compChoice === playerChoice) {
-        console.log('It\'s a tie!');
-        return 0;
-    //Determine the rest of the conditions. If the player wins a 'P' is returned
-    //if the computer wins a 'C' is returned
-    } else if (compChoice === 'rock' && playerChoice == 'scissors') {
-        console.log('Sorry, computer wins with rock vs. scissors.');
-        return 'C';
-    } else if (compChoice === 'rock' && playerChoice === 'paper') {
-        console.log('You win with paper vs rock.');
-        return 'P';
-    } else if (compChoice === 'paper' && playerChoice === 'rock') {
-        console.log('Sorry, computer wins with paper vs. rock.');
-        return 'C';
-    } else if (compChoice === 'paper' && playerChoice === 'scissors') {
-        console.log('You win with scissors vs. paper.');
-        return 'P';
-    } else if (compChoice === 'scissors' && playerChoice === 'rock') {
-        console.log('You win with rock vs scissors.');
-        return 'P';
-    } else if (compChoice === 'scissors' && playerChoice === 'paper') {
-        console.log('Sorry, computer wins with scissors vs. paper.');
-        return 'C';
+function playRound(playerChoice) {
+    //Reset play if it's the first round
+    if (roundNum === 1) {
+        resetPlay();
+    }
+    
+    //Get computer choice
+    let compChoice = getComputerChoice();
+    
+    //Determine winner
+    if (compChoice === 'R' && playerChoice == 'S') {
+        compTotal++;
+    } else if (compChoice === 'R' && playerChoice === 'P') {
+        playerTotal++;
+    } else if (compChoice === 'P' && playerChoice === 'R') {
+        compTotal++;
+    } else if (compChoice === 'P' && playerChoice === 'S') {
+        playerTotal++;
+    } else if (compChoice === 'S' && playerChoice === 'R') {
+        playerTotal++;
+    } else if (compChoice === 'S' && playerChoice === 'P') {
+        compTotal++;
+    }
+
+    updateScores();
+    updateRoundData(playerChoice,compChoice);
+    
+    //If roundNum is 5, reset to 1
+    if (roundNum === 5) {
+        displayWinner();
+        roundNum = 1;
+    } else {
+        roundNum++;
     }
 }
 
+function updateScores()
+ {
+    playerScore.textContent = playerTotal;
+    compScore.textContent = compTotal;
+ }
+
+ //Tracks the score
+function updateRoundData (player,comp) {
+    if (roundNum === 1) {
+        roundOneResult.textContent = `${playerTotal} | ${player} | 1 | ${comp} | ${compTotal}`;
+    } else if (roundNum === 2) {
+        roundTwoResult.textContent = `${playerTotal} | ${player} | 2 | ${comp} | ${compTotal}`;
+    } else if (roundNum === 3) {
+    roundThreeResult.textContent = `${playerTotal} | ${player} | 3 | ${comp} | ${compTotal}`;
+    } else if (roundNum === 4) {
+        roundFourResult.textContent = `${playerTotal} | ${player} | 4 | ${comp} | ${compTotal}`;
+    } else if (roundNum === 5) {
+        roundFiveResult.textContent = `${playerTotal} | ${player} | 5 | ${comp} | ${compTotal}`;
+    }
+ }
+
+function displayWinner() {
+    if (playerTotal > compTotal) {
+        playerScore.textContent = 'Win'
+        compScore.textContent = 'Lose'
+    } else if (playerTotal < compTotal) {
+    playerScore.textContent = 'Lose'
+    compScore.textContent = 'Win'
+    } else {
+        playerScore.textContent = 'Tied'
+        compScore.textContent = 'Tied'
+    }
+}
+
+function resetPlay() {
+    //Reset counters to 0
+    playerTotal = 0;
+    compTotal = 0;
+    updateScores();
+
+    //Clear the score tracker
+    roundOneResult.textContent = '';
+    roundTwoResult.textContent = '';
+    roundThreeResult.textContent = '';
+    roundFourResult.textContent = '';
+    roundFiveResult.textContent = '';
+}
 
 
-//Initialize gameplay
-game();
 
